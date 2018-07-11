34ea4aa4299b0991b907bb79bdaa6029990925dc
Michael-Rainabba Richardson <rainabba@gmail.com>
2018-07-11 12:04:44 -0700

    Initial commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#set -x #echo on
+CODE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+docker build -t rainabba/nodejs-8-wkhtmltopdf:0.12.4-$CODE_VERSION .
