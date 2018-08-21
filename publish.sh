00a0f9c98205426b961ef2d89c58146f188a1418
Michael Richardson <rainabba@gmail.com>
2018-08-21 11:14:20 -0700

    Fixed line-endings and version confusions then published 0.12.4-1.0.1 and latest

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
 echo on
 CODE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 docker push rainabba/nodejs-8-wkhtmltopdf:0.12.4-$CODE_VERSION
+docker push rainabba/nodejs-8-wkhtmltopdf:latest
